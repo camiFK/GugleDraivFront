0690bd09833a9bb970b43d277f0e2431bf450daa
GonzaloVelazquez <gonzalo.simon.velazquez@hotmail.com>
2024-11-21 03:12:25 -0300

    Interfaz Mejorada

--- front/src/components/FileUpload.jsx
+++ front/src/components/FileUpload.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import React from "react";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
-function FileUpload({ onUpload }) {
+
+function FileUpload({ onUpload, setErrorMessage }) {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
-  const handleUpload = () => {
-    if (file) {
-      onUpload(file);
+  const handleSubmit = () => {
+    if (!file) {
+      // Si no se seleccionó un archivo, muestra un mensaje de advertencia
+      setErrorMessage({
+        message: "Por favor, selecciona un archivo antes de subir.",
+        severity: "error",
+      });
+      return;
     }
+    // Si hay un archivo, pasa el archivo a la función onUpload
+    onUpload(file);
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      <Button variant="contained" color="primary" onClick={handleUpload}>
-        Subir archivo
+      <Button onClick={handleSubmit} variant="contained" color="primary" style={{ marginTop: "12px", width: "100%" }}>
+        Subir Archivo
       </Button>
     </div>
   );
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">
-          Gugle Draiv
-        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          {/* Agregamos el logo como imagen */}
+          <img
+            src="https://1000marcas.net/wp-content/uploads/2021/06/Google-Drive-logo.png"
+            alt="Google Drive Logo"
+            style={{ width: 50, height: 30, marginRight: 5 }}
+          />
+          <Typography variant="h6">Gugle Draiv</Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
